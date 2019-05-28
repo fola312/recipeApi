d3f5041af05e51be04835f2b50602cfaba17dd67
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-05-28 11:50:59 -0400

    First

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const knex = require('knex');
+const cors = require('cors');
+
+
+const app = express();
+
+
+const postgres = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test',
+      database : 'Whats-for-dinner'
+    }
+  });
+  
+app.use(cors())
+
+
+app.get('/', (req, res) => {const randomRecipe = postgres.select('*').from('recipes')
+.then(data => {res.send((data[Math.floor(Math.random()*data.length)]))
+
+})})
+
+app.listen(3000, ()=> {
+    console.log("Up and at em'")
+})
