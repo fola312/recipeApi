9e00ff51d20cb61f1cf5432a9c3cf296b3f42772
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-05-28 13:49:45 -0400

    connectionString

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const postgres = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true,
     }
   });
